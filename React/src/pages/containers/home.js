842e9b25992fa11db96f385d6f89c5487ac2ec80
Ruben E Acosta <eldragondera1@gmail.com>
2018-02-11 18:36:54 -0500

    Manejando errores

--- React/src/pages/containers/home.js
+++ React/src/pages/containers/home.js
@@ -4,16 +4,19 @@ import Categories from '../../categories/componentes/categories'
 import Related from '../componetes/related';
 import ModalContainer from '../../widgets/containers/modal-container';
 import Modal from '../../widgets/componentes/modal';
+import HandleError from '../../error/containers/handle-error';
 
 class Home extends Component {
   state = {      //constructor
     modalVisible: false,
   }
+  
   handleOpenModal = ()=>{
     this.setState({
       modalVisible: true,
     })
   }
+
   handleCloseModal = (event)=>{
     this.setState({
       modalVisible: false,
@@ -22,21 +25,23 @@ class Home extends Component {
 
   render(){
     return (
-      <HomeLayout>
-        <Related/>
-        <Categories
-          categories={this.props.data.categories}
-          handleOpenModal={this.handleOpenModal}
-        />
-        {
-          this.state.modalVisible &&  /*si se cumple, renderiza*/
-          <ModalContainer>
-            <Modal handleClick={this.handleCloseModal}>
-              <h2>MODAL</h2>
-            </Modal>
-          </ModalContainer>
-        }
-      </HomeLayout>
+      <HandleError>
+        <HomeLayout>
+          <Related/>
+          <Categories
+            categories={this.props.data.categories}
+            handleOpenModal={this.handleOpenModal}
+          />
+          {
+            this.state.modalVisible &&  /*si se cumple, renderiza*/
+            <ModalContainer>
+              <Modal handleClick={this.handleCloseModal}>
+                <h2>MODAL</h2>
+              </Modal>
+            </ModalContainer>
+          }
+        </HomeLayout>
+      </HandleError>
     )
   }
 }
