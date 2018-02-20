6834e206321eecf4a0d8672f333f6d8a5735dc9d
Ruben E. Acosta <eldragondera1@gmail.com>
2018-02-19 21:40:46 -0500

    webpack imagenes

---
+++ webpack/url-loader-images/index.js
@@ -0,0 +1,16 @@
+import './estilos.css';
+import { firstMessage, delay } from './message.js';
+import platziImg from './platzi.png'
+
+document.write(firstMessage);
+
+console.log('Hola webpack');
+delay();
+
+const img = document.createElement('img');
+img.setAttribute('src', platziImg);
+img.setAttribute('width', 50);
+img.setAttribute('height', 50);
+document.body.append(img);
+
+console.log('webpack es genial');
