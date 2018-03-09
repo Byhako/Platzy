9adb54b2014cbd81a65d6ce81fe904535bc88900
Ruben E. Acosta <eldragondera1@gmail.com>
2018-03-09 13:03:47 -0500

    directivas angular

--- angular/platzisquare/src/app/app.component.ts
+++ angular/platzisquare/src/app/app.component.ts
@@ -9,12 +9,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'PlatziSquare';
   lugares:any = [
-    {activate: true, nombre:'Floristeria'},
-    {activate: true, nombre:'Donas'},
-    {activate: false, nombre:'Veterinaria'},
-    {activate: true, nombre:'Sistemas'},
+    {cercania: 1, distancia: 1, activate: true, nombre:'Donas'},
+    {cercania: 2, distancia: 5, activate: false, nombre:'Veterinaria'},
+    {cercania: 1, distancia: 2, activate: true, nombre:'Floristeria'},
+    {cercania: 3, distancia: 4, activate: true, nombre:'Sistemas'},
   ];
-
   lat:number = 3.5912464;
   lng:number = -72.4880479;
 
