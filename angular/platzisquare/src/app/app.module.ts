298999b4e4ca5d12f337efc5038142208e6f864d
Ruben E. Acosta <eldragondera1@gmail.com>
2018-03-12 13:25:52 -0500

    implementando rutas en angular

--- angular/platzisquare/src/app/app.module.ts
+++ angular/platzisquare/src/app/app.module.ts
@@ -2,16 +2,19 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { AgmCoreModule } from '@agm/core'
-import { Routes } from '@angular/router'
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component'
+import { LugaresComponent } from './lugares/lugares.component'
 import { ResaltarDirective } from './directives/resaltar.directive'
 import { ContarClicks } from './directives/contar-click.directive'
+import { DetalleComponent } from './detalle/detalle.component'
 
+// Configurando el enrutador
 const appRoutes: Routes = [
-  {path: '', component: AppComponent},
-  {path: 'lugares', component: AppComponent}
-
+  {path: '', component: LugaresComponent},
+  {path: 'lugares', component: LugaresComponent},
+  {path: 'detalle', component: DetalleComponent},
 ]
 
 @NgModule({
@@ -19,9 +22,12 @@ const appRoutes: Routes = [
     AppComponent,
     ResaltarDirective,
     ContarClicks,
+    DetalleComponent,
+    LugaresComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBudbL3aWWnwrmaeXAVlTiMOPDZbiDnVo0'
@@ -30,4 +36,5 @@ const appRoutes: Routes = [
   providers: [],              // servicios necesarios
   bootstrap: [AppComponent]   // componente de inicio
 })
+
 export class AppModule { }
---
+++ angular/platzisquare/src/app/detalle/detalle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-detalle',
+  templateUrl: './detalle.component.html',
+})
+
+export class DetalleComponent {
+
+}
+
