c21ce37600b72c3e0e25444f5d141fca19fa8e94
Ruben E. Acosta <eldragondera1@gmail.com>
2018-03-12 11:43:28 -0500

    Bootstrap angular

--- angular/platzisquare/src/app/app.module.ts
+++ angular/platzisquare/src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AgmCoreModule } from '@agm/core';
-import { ResaltarDirective } from './directives/resaltar.directive';
-import { ContarClicks } from './directives/contar-click.directive';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { AgmCoreModule } from '@agm/core'
+import { Routes } from '@angular/router'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { ResaltarDirective } from './directives/resaltar.directive'
+import { ContarClicks } from './directives/contar-click.directive'
 
+const appRoutes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'lugares', component: AppComponent}
+
+]
 
 @NgModule({
   declarations: [             // donde agregamos los componentes
