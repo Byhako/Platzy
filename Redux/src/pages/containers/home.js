870661094205394cd5b1eadefa376dd58a7d7642
Ruben E. Acosta <eldragondera1@gmail.com>
2018-03-15 17:09:25 -0500

    modificando estado inmutable

--- Redux/src/pages/containers/home.js
+++ Redux/src/pages/containers/home.js
@@ -7,6 +7,7 @@ import Modal from '../../widgets/componentes/modal';
 import HandleError from '../../error/containers/handle-error';
 import VideoPlayer from '../../player/container/video-players';
 import { connect } from 'react-redux';
+import { List as list } from 'immutable'
 
 class Home extends Component {
   state = {      //constructor
@@ -63,9 +64,17 @@ function mapStateToProps(state, props){
   const categories = state.get('data').get('categories').map((categoryId) => {
     return state.get('data').get('entities').get('categories').get(categoryId)
   })
+  let searchResults = list()
+  const search = state.get('data').get('search')
+  if(search) {
+    const mediaList = state.get('data').get('entities').get('media')
+    searchResults = mediaList.filter((item) => {
+      return item.get('author').toLowerCase().includes(search.toLowerCase())
+    }).toList()
+  }
   return{
     categories: categories,
-    search: state.get('data').get('search')
+    search: searchResults
   }
 }
 
--- Redux/src/reducers/data.js
+++ Redux/src/reducers/data.js
@@ -4,26 +4,27 @@ import schema from '../schemas/index'
 const initialState =fromJS({
   entities: schema.entities,
   categories: schema.result.categories,
-  search: [],
+  search: '',
 })
 
 function data(state = initialState, action) {
   switch(action.type) {
     case 'SEARCH_VIDEO':{
-      //action.payload.query
-      let results = []
+    //   //action.payload.query
+    //   let results = []
 
-      if(action.payload.query) {
-        const list = state.data.categories[2].playlist
-        results = list.filter((item) => {
-          return item.author.includes(action.payload.query)
-        })
-      }
+    //   if(action.payload.query) {
+    //     const list = state.data.categories[2].playlist
+    //     results = list.filter((item) => {
+    //       return item.author.includes(action.payload.query)
+    //     })
+    //   }
 
-      return {
-        ...state,
-        search: results
-      }
+    //   return {
+    //     ...state,
+    //     search: results
+    //   }
+      return state.set('search', action.payload.query)
     }
     default:
       return state
