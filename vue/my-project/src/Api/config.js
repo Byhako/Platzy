54782765141f32a13448acac5eab2881cd86b79a
Ruben E Acosta <eldragondera1@gmail.com>
2018-08-04 15:49:15 -0500

    curso basico de vue

---
+++ vue/my-project/src/Api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appname: 'platziMusic',
+  API_KEY: '64013f64b914ad9149d56acd86a4df86',
+  secret: '43172609a8c54385abd3a5cd60c081ca',
+  registeredTo: 'byhako'
+}
---
+++ vue/my-project/src/components/Artist.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  li
+    h2
+      a(:href="artist.url" target="_blank") {{ artist.name }}
+    img(:src="artist.image[2]['#text']")
+</template>
+
+<script>
+  export default {
+    name: 'Artist',
+    props: ['artist']
+  }
+</script>
+
+
+<style lang="stylus" scoped>
+li
+  display block
+  margin 10px 15px
+a
+  color #42b983
+</style>
