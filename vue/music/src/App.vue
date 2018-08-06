2956804cf8514b4e7ace07334b0a61fb4bcc6d47
Ruben E. Acosta <eldragondera1@gmail.com>
2018-08-06 11:34:13 -0500

    configuracion basica vue

---
+++ vue/music/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <a class="button is-primary">Primary</a>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../node_modules/bulma-stylus/bulma.styl'
+#app 
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+
+h1
+  color: #204A87
+
+
+</style>
