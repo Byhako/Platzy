fb3913d40a6065cbe14dd399a4a95fde3bf9364b
Ruben E. Acosta <eldragondera1@gmail.com>
2018-08-21 17:30:44 -0500

    peticiones a servidor con fetch

--- native_react/videoApp/App.js
+++ native_react/videoApp/App.js
@@ -5,8 +5,16 @@ import Home from './src/screems/Home'
 import Header from './src/sections/Header'
 import Suggestions from './src/video/suggestionsList'
 
+import API from './utils/api'
+
 //type Props = {}
 export default class App extends Component {
+
+  async componentDidMount () {
+    const movies = await API.getSuggestion(10)
+    console.log('m:', movies)
+  }
+
   render() {
     return (
       <Home>
