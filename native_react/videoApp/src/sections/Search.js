ef24a70d5d8060da63e635920a4a30b4d4f05b8d
Ruben E. Acosta <eldragondera1@gmail.com>
2018-08-29 14:57:53 -0500

    buscando pelis

--- native_react/videoApp/src/sections/Search.js
+++ native_react/videoApp/src/sections/Search.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import { TextInput, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
 
+import API from '../../utils/api'
 
 class Search extends Component {
   state = {
     text: ''
   }
-  handleSubmit = () => {
+  handleSubmit = async () => {
     console.log(this.state.text)
-
+    const movie = await API.searchMovie(this.state.text)
+    this.props.dispatch({type: 'SET_SELECTED_MOVIE', movie:movie[0]})
   }
   handleChangeText = (text) => {
     this.setState({
@@ -39,4 +42,5 @@ const styles = StyleSheet.create({
   }
 })
 
-export default Search
+
+export default connect()(Search)
