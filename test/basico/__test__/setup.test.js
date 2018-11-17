e7f7c3510318f5986623b80906340af6cb63fc85
Ruben E. Acosta <eldragondera1@gmail.com>
2018-11-17 11:52:55 -0500

    setup de pruebas

---
+++ test/basico/__test__/setup.test.js
@@ -0,0 +1,18 @@
+// despues de cada prueba
+
+afterEach(() => console.log('Despues de cada prueba.'))
+afterAll(() => console.log('Despues de todas las pruebas.'))
+
+// antes de cada prueba
+
+beforeAll(() => console.log('Antes de todas las pruebas.'))
+beforeEach(() => console.log('Antes de cada prueba.'))
+
+describe('Prepara antes de ejecutar', () => {
+  test('Es verdadero', () => {
+    expect(true).toBeTruthy()
+  })
+  test('Es falso', () => {
+    expect(false).toBeFalsy()
+  })
+})
