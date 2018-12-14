782bda9e34534496a2466f8e2499406245ec6ea5
Ruben E. Acosta <eldragondera1@gmail.com>
2018-12-13 21:42:59 -0500

    modulos ruby

---
+++ ruby/modulos.rb
@@ -0,0 +1,13 @@
+module Reports
+  class ExcelReporter
+    def build
+      puts "Generating excel report"
+    end
+  end
+
+  class EmailReporter
+  end
+end
+
+excel_report = Reports::ExcelReporter.new
+excel_report.build
