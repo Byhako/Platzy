b0cc62992248617e045735d2dd43141418a563d2
Ruben E. Acosta <eldragondera1@gmail.com>
2019-01-11 22:44:30 -0500

    terminando pruebas con ava

--- node/avanzado/verse-db/tests/agent-tests.js
+++ node/avanzado/verse-db/tests/agent-tests.js
@@ -14,17 +14,38 @@ let MetricStub = {
   belongsTo: sinon.spy()
 }
 
-// let single = Object.assign({}, agentFixtures.single)
-let id = 1
+let single = Object.assign({}, agentFixtures.single)
+const id = 1
+const uuid = 'yyy-yyy-yyy'
 let AgentStub = null
 let db = null
 let sandbox = null
 
+const uuidArgs = {
+  where: {
+    uuid
+  }
+}
+
 test.beforeEach(async () => {
   sandbox = sinon.createSandbox()
+
   AgentStub = {
     hasMany: sandbox.spy()
   }
+
+  // Model findOne
+  AgentStub.findOne = sandbox.stub()
+  AgentStub.findOne.withArgs(uuidArgs).returns(Promise.resolve(agentFixtures.byUuid(uuid)))
+
+  // Model update
+  AgentStub.update = sandbox.stub()
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single))
+
+  // Model findById Stub
+  AgentStub.findById = sandbox.stub()
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
     './models/metric': () => MetricStub
@@ -50,5 +71,19 @@ test.serial('setup', t => {
 test.serial('AgentFindById', async t => {
   let agent = await db.Agent.findById(id)
 
+  t.true(AgentStub.findById.called, 'findById should be called on model')
+  t.true(AgentStub.findById.calledOnce, 'findById should be called once')
+  t.true(AgentStub.findById.calledWith(id), 'findById should be called with id')
+
   t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
+})
+
+test.serial('Agent#createOrUpdate - exists', async t => {
+  let agent = await db.Agent.createOrUpdate(single)
+
+  t.true(AgentStub.findOne.called, 'findOne should be called on model')
+  t.true(AgentStub.findOne.calledTwice, 'findOne should be called Twice')
+  t.true(AgentStub.update.calledOnce, 'update should be called Once')
+
+  t.deepEqual(agent, single, 'Agent should be the same')
 })
