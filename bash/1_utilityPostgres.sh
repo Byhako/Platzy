172d3fbdba58eeb7a80a0e2dfa8764f13bca16c5
Ruben E Acosta <eldragondera1@gmail.com>
2019-02-06 09:05:15 -0500

    backend python

---
+++ bash/1_utilityPostgres.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Programa para realizar algunas operaciones utilitarias de postgres
+# chmod 421 421 421  -> rwx
+# chmod 764 1_utillityPostgres.sh
+# chmod +x 1_utillityPostgres.sh
+echo "Hola, mi primer script bash"
---
+++ bash/2_variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Declaracion de variables
+
+option=0
+nombre=Marco
+
+echo "Option: $option y Nombre $nombre"
